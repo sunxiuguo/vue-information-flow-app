ee6d9a1e6a8458a84ca93c0c33af1c9a39b4f69b
sunxg <sunxg@fenbi.com>
2020-05-26 20:25:59 +0800

    ADD: media query

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,50 @@
         }
     }
 }
+
+html {
+    width: 100%;
+    height: 100%;
+    font-size: 10px !important;
+}
+
+@media only screen and (max-width: 338px) {
+    html {
+        font-size: 9px !important;
+    }
+}
+
+// iPhone Plus
+@media only screen and (min-width: 411px) {
+    html {
+        font-size: 11px !important;
+    }
+}
+
+// Android Pad
+@media only screen and (min-width: 450px) {
+    html {
+        font-size: 12px !important;
+    }
+}
+
+@media only screen and (min-width: 525px) {
+    html {
+        font-size: 14px !important;
+    }
+}
+
+// Nexus 7
+@media only screen and (min-width: 600px) {
+    html {
+        font-size: 16px !important;
+    }
+}
+
+// iPad or Browser
+@media only screen and (min-width: 768px) {
+    html {
+        font-size: 20px !important;
+    }
+}
 </style>
