927f131455009a94368ef953d0f0037f92cf2592
sunxg <sunxg@fenbi.com>
2020-05-28 20:11:55 +0800

    ADD: loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="mask">
+        <div class="spinner">
+            <div
+                v-for="item in list"
+                :key="item"
+                :class="`rect${item}`"
+                :style="getStyle(item)"
+            ></div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+    components: {}
+})
+export default class Loading extends Vue {
+    list = [1, 2, 3, 4, 5];
+
+    getStyle(index: number) {
+        const seconds = -1 + index / 10;
+        return { "animation-delay": `${seconds}s` };
+    }
+}
+</script>
+<style lang="less" scoped>
+@import "../style/index.less";
+
+.mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+
+    z-index: 10000;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .spinner {
+        display: flex;
+        width: 5rem;
+        height: 6rem;
+
+        div {
+            margin-left: 0.5rem;
+            background-color: @main-color;
+            height: 100%;
+            width: 0.6rem;
+            animation: stretchdelay 1.2s infinite ease-in-out;
+            border-radius: 0.5rem;
+        }
+    }
+}
+
+@keyframes stretchdelay {
+    0%,
+    40%,
+    100% {
+        transform: scaleY(0.4);
+        -webkit-transform: scaleY(0.4);
+    }
+    20% {
+        transform: scaleY(1);
+        -webkit-transform: scaleY(1);
+    }
+}
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,24 +1,33 @@
 <template>
-    <div class="detail" v-if="imgSrc">
-        <img :src="imgSrc" alt="banner" class="detail-banner" />
-        <div class="content-container">
-            <h1>{{ title }}</h1>
-            <div class="content">{{ content }}</div>
+    <div>
+        <loading v-if="loading" />
+        <div class="detail" v-if="imgSrc">
+            <img :src="imgSrc" alt="banner" class="detail-banner" />
+            <div class="content-container">
+                <h1>{{ title }}</h1>
+                <div class="content">{{ content }}</div>
+            </div>
         </div>
-    </div>
-    <div v-else>
-        这里没有数据哦~
+        <div v-else>
+            这里没有数据哦~
+        </div>
     </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Services from "../services";
+import Loading from "../components/Loading.vue";
 
-@Component
+@Component({
+    components: {
+        Loading
+    }
+})
 export default class Detail extends Vue {
     title = "";
     content = "";
     imgSrc = "";
+    loading = true;
 
     get id() {
         return this.$route.params.id;
@@ -29,6 +38,9 @@ export default class Detail extends Vue {
         this.title = res.title;
         this.content = res.content;
         this.imgSrc = res.image;
+        setTimeout(() => {
+            this.loading = false;
+        }, 1000);
     }
 }
 </script>
